11244bb4a79fe8325f19f6c206a629cdaf5384fa
Franck <franck.c.moreira@gmail.com>
2024-11-15 21:56:08 -0300

    Adicionando alias para apagar

--- comandos_basicos.sh
+++ comandos_basicos.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-alias atualizar="sudo apt-get update && sudo apt-get upgrade"
+alias atualizar="sudo apt-get update && sudo apt-get upgrade && sudo snap refresh"
 
 alias desligar="sudo shutdown -h now"
 
 alias reiniciar="sudo shutdown -r now"
+
+alias apagar="rm -rf"
+
+alias deletar="rm -rf"
+
+alias remover="rm -rf"
